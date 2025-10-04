662e16a0c17a4d8e0a5b37d7b1d7d5d4fabcfcbc
asudeyilmaz <asudeyilmaz76@gmail.com>
2025-10-04 20:06:25 +0300

    challenge added

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Fm-results-summary-component
+
+# This is my solution to the [Results summary component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV). 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+
+
+### Screenshot
+
+![](/assets/images/1.png)
+![](/assets/images/2.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flex
+- Mobile-first workflow
+
+
+
+
+## Author
+
+- Frontend Mentor - [@asudeyilmaz](https://www.frontendmentor.io/profile/asudeyilmaz)
+
+
+
+
+
---
+++ app.js
@@ -0,0 +1,39 @@
+const reactionScore = document.querySelector("#reactionScore");
+const memoryScore = document.querySelector("#memoryScore");
+const verbalScore = document.querySelector("#verbalScore");
+const visualScore = document.querySelector("#visualScore");
+const result = document.querySelector("#result");
+const toggle = document.querySelector(".toggle");
+const img = document.querySelectorAll("img");
+  const body= document.querySelector("body");
+const li = document.querySelectorAll(".stats li");
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const memoryNumber = Number((memoryScore.textContent = data[1].score));
+    const verbalNumber = Number((verbalScore.textContent = data[2].score));
+    const visualNumber = Number((visualScore.textContent = data[3].score));
+    const reactionNumber = Number((reactionScore.textContent = data[0].score));
+    const average = Math.floor(
+      (reactionNumber + memoryNumber + verbalNumber + visualNumber) / 4
+    );
+    result.textContent = average;
+  });
+
+  toggle.addEventListener("click", changeCss);
+
+  function changeCss(){
+  
+    if(toggle.checked==true){
+      body.classList.toggle("dark");
+      for(let value of img){
+        value.style.display="none";
+      }
+
+    }else{
+      body.className="body"
+      for(let value of img){
+        value.style.display= "block"
+      }
+    }
+  }
